90e11bae6518dab214d3f112bb6c79caf1ca191d
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-22 15:40:19 -0400

    v1

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
-
+app.use("/", function(req,res){
+    res.send("home");
+});
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
