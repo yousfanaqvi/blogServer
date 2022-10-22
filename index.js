24dad041807021318bbdb22240c239673266dfcb
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-22 16:57:41 -0400

    v2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const customer = require("./api/customer");
+const stripe= require("stripe")(process.env.stripkey);
+require('dotenv').config();
 
 app.use(express.json({ extended: false }));
 
