31526ab8225f36ebf03daaa88b27039f5f33b962
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-22 17:13:45 -0400

    v2.1

--- api/customerSchema.js
+++ api/customerSchema.js
@@ -1,6 +1,7 @@
 // login page (register schema)
 const mongoose= require("mongoose");
-mongoose.connect("mongodb+srv://yousfa:Amazon123@cluster0.nfqggsr.mongodb.net/storeDB?retryWrites=true&w=majority",{
+
+mongoose.connect(process.env.MONGO_URI,{
  
 })
 const registerSchema=new mongoose.Schema({
