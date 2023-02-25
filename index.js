e669a2541d6201560ec7189443a0d598aedc8704
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2023-02-25 01:19:25 -0500

    version 4.3

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 const user = require("./api/user");
 app.use(express.json({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/", user);
 
 
