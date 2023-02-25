6c3018953aafbcfa1f14542f00739c2fd5c5dd9d
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2023-02-24 21:00:36 -0500

    version 2.2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require("express");
 const cookieParser = require('cookie-parser')
 const session = require('express-session');
@@ -6,6 +5,7 @@ const passport=require('passport');
 const path = require("path");
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
 const bodyParser=require("body-parser");
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -13,50 +13,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin:'https://blog-6uviut0if-yousfanaqvi.vercel.app',
 }));
-// app.use(cors({
-//   origin: "*", // allow to server to accept request from different origin
-//   methods: ['GET','POST','DELETE'],
-//   credentials: true, // allow session cookie from browser to pass through
-//   "Access-Control-Allow-Credentials": true
-
-// }));
 
 app.use(cookieParser('keyboard cat'));
 app.use(session({
-  secret: 'keyboard cat',
+  secret: prpcess.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie : { httpOnly: true, secure : false}
 }))
 app.use(passport.initialize());
 app.use(passport.session());
-
-// app.use(cors({
-//   origin:'https://shoppingcart-beta.vercel.app',
-// }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// const customer = require("./api/customer");
-
 app.use(express.json({ extended: false }));
-
-// app.use("/", customer);
-
-// const PORT = process.env.PORT || 8080;
 const user = require("./api/user");
 app.use("/", user);
 
-// app.use(express.static(path.join(__dirname, "./frontend/build")));
-// app.get("*", function (_, res) {
-//   res.sendFile(
-//     path.join(__dirname, "./frontend/build/index.html"),
-//     function (err) {
-//       if (err) {
-//         res.status(500).send(err);
-//       }
-//     }
-//   );
-// });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running on port ${port}`));
 
