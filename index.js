dab322029968bda72f82a6e4f786155dd08e6d2d
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2023-02-25 12:25:43 -0500

    version 4.5

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin:'https://blog-gayl03f5r-yousfanaqvi.vercel.app',
 }));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cookieParser('keyboard cat'));
 app.use(session({
   secret: process.env.SECRET,
