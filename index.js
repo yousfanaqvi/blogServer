cddcaddacd01f337d2c6263d27261f9b5e6b969c
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2023-02-25 00:11:17 -0500

    version 4.1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:'https://blog-ocus4e2aa-yousfanaqvi.vercel.app',
+  origin:'https://blog-gayl03f5r-yousfanaqvi.vercel.app',
 }));
 
 app.use(cookieParser('keyboard cat'));
